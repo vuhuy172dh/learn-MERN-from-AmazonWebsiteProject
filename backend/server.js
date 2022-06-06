4a414eab1c2bc43cc961d2faacceb581d11ba34c
Vu_Huy <quochuy123dh@gmail.com>
2022-06-06 22:53:35 +0700

    Create-SignIn-Backend-API

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -17,8 +17,15 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.port || 5000;
 app.listen(port, () => {
